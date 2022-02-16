f597fecbfd4256ae61c850fa12538aaef24fe81f
Alan Poveda <alsepoveda@gmail.com>
2022-02-15 22:06:45 -0300

    Config _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,20 @@
-import Document from 'next/document';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>AlanPoveda - Blog</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
