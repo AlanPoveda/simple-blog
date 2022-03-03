22b8cf65765f7a22876bb6be2f0f1f61c87280b0
Alan Poveda <alsepoveda@gmail.com>
2022-03-03 16:01:04 -0300

    Logo image insert

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link';
 import styled from './header.module.scss';
 
-export default function Header(): any {
+export default function Header(): JSX.Element {
   return (
     <div className={styled.header}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </Link>
       <h2>
         spacetraveling<span>.</span>
       </h2>
